012dc9f1a6d820316fc502f010390f8892001533
Batya02 <magomedov.abdul20012@gmail.com>
2021-09-26 01:10:15 +0300

    new urls

--- web/wgb/urls.py
+++ web/wgb/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", index, name="index"),
     path("users", users, name="users"),
     path("more-info/<int:user_id>", more_info_user),
+    path("more-info-for-user/<int:user_id>", more_info_for_user),
     path("login/", login),
     path("profile/<int:user_id>", user_profile, name="user_profile"),
     path("<int:user_id>/shops", shops, name="shops"),
